9ebefd0f5ee1596220f15e02bf7a6de8fb300025
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2026-04-28 12:14:52 +0900

    Update core.py INSERT,UPDATE,DELETE query

--- Oracle/SQLAlchemyInPython/core.py
+++ Oracle/SQLAlchemyInPython/core.py
@@ -22,14 +22,44 @@ try:
     inspector = inspect(engine)
     columns = inspector.get_columns(table)
 
-    print("=" * 20)
+    print("=" * 20) # 4-1. INSERT 문
+    print(f"INSERT INTO {table} (")
+    for no, column in enumerate(columns):
+        if no == 0:
+            print(f"\t{column['name']}")
+        else:
+            print(f",\t{column['name']}")
+    print(") VALUES (")
+    for no, column in enumerate(columns):
+        if no == 0:
+            print(f"\t:{column['name']}")
+        else:
+            print(f",\t:{column['name']}")
+    print(")")
+    for no, column in enumerate(columns):
+        if no == 0:
+            print("[{" + f"\'{column['name']}\': \'\'", end="")
+        else:
+            print(f", \'{column['name']}\': \'\'", end="")
+    print("}]")
+    print("=" * 20) # 4-2. SELECT 문
     for no, column in enumerate(columns):
         if no == 0:
             print(f"SELECT  {column['name']}")
         else:
             print(f",\t{column['name']}")
     print(f"FROM\t{table}")
-    print("=" * 20 + "\n")
+    print("=" * 20) # 4-3. UPDATE 문
+    for no, column in enumerate(columns):
+        if no == 0:
+            print(f"UPDATE {table}\nSET\t{column['name']} = \'\'")
+        else:
+            print(f",\t{column['name']} = \'\'")
+    print("WHERE\ttest = \'\'")
+    print("=" * 20) # 4-4. DELETE 문
+    print(f"DELETE FROM {table}")
+    print("WHERE\ttest = \'\'")
+    print("=" * 20)
 except Exception as e:
     print(f"Error: {e}")
 finally:
@@ -40,9 +70,26 @@ finally:
 """ 예시)
 
 ====================
+INSERT INTO test_table (
+        no
+,       name
+) VALUES (
+        :no
+,       :name
+)
+[{'no': '', 'name': ''}]
+====================
 SELECT  no
 ,       name
 FROM    test_table
+====================
+UPDATE test_table
+SET     no = ''
+,       name = ''
+WHERE   test = ''
+====================
+DELETE FROM test_table
+WHERE   test = ''
 ====================
 
 """
